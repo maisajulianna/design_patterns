1416ed7b7aab40588be8dfb7b5b8d89b53dc353d
maisajulianna <maisa.osterman@gmail.com>
2025-01-27 23:07:54 +0200

    singleton

---
+++ singleton/Logger.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+public class Logger {
+    private static Logger instance;
+    private PrintWriter writer;
+
+    private Logger() {
+        createFile("log.txt");
+    }
+
+    private void createFile(String name) {
+        try {
+            File file = new File(name);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            writer = new PrintWriter(new FileWriter(file, true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void setFileName(String name) {
+        close();
+        createFile(name);
+    }
+
+    public void write(String text) {
+        if (writer != null) {
+            writer.println(text);
+            writer.flush();
+        } else {
+            System.out.println("No writer initialized");
+        }
+
+    }
+
+    public void close() {
+        if (writer != null) {
+            writer.close();
+        }
+    }
+}
---
+++ singleton/MainLogger.java
@@ -0,0 +1,12 @@
+
+
+public class MainLogger {
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.setFileName("new_log.txt");
+        logger.write("Simulation started");
+        logger.write("Processing data...");
+        logger.write("Simulation finished");
+        logger.close();
+    }
+}
